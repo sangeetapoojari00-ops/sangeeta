27c3827d50b90a49f3a8fc1296c354eb5edce0bd
labuser <labuser@ip-172-31-27-152.ap-south-1.compute.internal>
2025-12-11 10:11:08 +0000

    hi

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -12,17 +12,13 @@ public class BaseTest {
 
     @BeforeTest
     public void setup() {
-        // *** IMPORTANT: UPDATE THIS PATH ***
-//        System.setProperty("webdriver.chrome.driver", "/path/to/your/chromedriver"); 
-        
+      
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     
         
         driver.get("https://www.flipkart.com/");
-        
-        // Handle pop-up
-      
+     
     }
 
     @AfterTest
--- src/main/java/ok/FlipkartHomePage.java
+++ src/main/java/ok/FlipkartHomePage.java
@@ -9,17 +9,16 @@ public class FlipkartHomePage {
     private WebDriver driver;
 
     // Locators
-    private By searchInput = By.xpath("//input[@placeholder='Search for Products, Brands and More']");
-    private By firstFilterLowToHigh = By.xpath("//div[text()='Price -- Low to High']");
+   By searchInput = By.xpath("//input[@placeholder='Search for Products, Brands and More']");
+   By firstFilterLowToHigh = By.xpath("//div[text()='Price -- Low to High']");
     // ... (Add all other locators here)
 
     public FlipkartHomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    // Actions
-    public void searchProduct(String productName) {
-        driver.findElement(searchInput).sendKeys(productName + Keys.ENTER);
+        public void searchProduct() {
+        driver.findElement(searchInput).sendKeys("doll" + Keys.ENTER);
     }
     
     public void clickLowToHighFilter() throws InterruptedException {
@@ -27,17 +26,10 @@ public class FlipkartHomePage {
         driver.findElement(firstFilterLowToHigh).click();
     }
    
-    // ... (Add all other action methods here)
+
     
     public boolean isSearchResultsDisplayed() {
         return driver.getTitle().contains("doll");
     }
-
-//	public void navigateToLogin() {
-//		driver.findElement(By.xpath(""))
-		// TODO Auto-generated method stub
-		
-	
-
 
 }
